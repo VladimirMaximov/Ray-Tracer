4d28a3bd9af700d255dff97ccd03db4b36c05dc8
VladimirMaximov <vladimirmaximov2004@mail.ru>
2023-12-03 17:09:21 +0300

    Create base shadows

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from dataclasses import dataclass
 import pygame as pg
 import numpy as np
+from numpy.linalg import norm
 import sys
 
 # Инициализация PyGame
 pg.init()
 
 # Константы
-C_w = pg.display.get_desktop_sizes()[0][0] - 100
+C_w = 500
 C_w_half = C_w // 2
-C_h = pg.display.get_desktop_sizes()[0][1] - 100
+C_h = 500
 C_h_half = C_h // 2
 C_w_to_C_h = C_w / C_h
 FPS = 30
-BACKGROUND_COLOR = (255, 255, 255)
+BACKGROUND_COLOR = np.array([255, 255, 255])
 
 # Окно игры
 screen = pg.display.set_mode((C_w, C_h))
@@ -34,7 +35,7 @@ pg.display.set_icon(pg.image.load("icon.jpg"))
 
 # Функция, которая по координатам пикселя задает ему цвет
 def paintPixel(x_coord: int, y_coord: int, pixel_color):
-    pixel_array[C_w_half + x_coord, C_h_half - y_coord] = pixel_color
+    pixel_array[C_w_half + x_coord, C_h_half - y_coord] = tuple(pixel_color)
 
 
 # Камера - точка начала лучей
@@ -46,16 +47,46 @@ camera = np.array([0, 0, 0])
 class Sphere:
     center: np.array
     radius: float
-    color: tuple
+    color: np.array
 
+
+# Класс света, тип света 1 означает общий свет,
+# тип света 2 - точечный свет,
+# тип света 3 - направленный свет
+@dataclass
+class Light:
+    type_of_light: int
+    intensity: float
+    position: np.array
+    direction: np.array
+
+
+# Класс объекта, тип объекта 1 - это сфера
+@dataclass
+class Object:
+    type_of_object: int
+    object_on_scene: Sphere
+
+# Класс сцена, основной объект,
+# который будет прорисовываться на экране
+@dataclass
+class Scene:
+    lights: list[Light]
+    objects: list[Object]
 
 # Сцена с объектами
-scene = [
-    Sphere(np.array([0, -1, 3]), 1, (255, 0, 0)),
-    Sphere(np.array([2, 0, 4]), 1, (0, 0, 255)),
-    Sphere(np.array([-2, 0, 4]), 1, (0, 255, 0))
-]
-
+scene = Scene(
+    lights=[
+        Light(1, 0.2, None, None),
+        Light(2, 0.6, np.array([2, 1, 0]), None),
+        Light(3, 0.2, None, np.array([1, 4, 4]))
+    ],
+    objects=[
+        Object(1, Sphere(np.array([0, -1, 3]), 1, np.array([255, 0, 0]))),
+        Object(1, Sphere(np.array([2, 0, 4]), 1, np.array([0, 0, 255]))),
+        Object(1, Sphere(np.array([-2, 0, 4]), 1, np.array([0, 255, 0])))
+    ]
+)
 
 # Поиск пересечения луча со сферой
 def intersect_ray_sphere(camera_position, direction, sphere):
@@ -76,14 +107,42 @@ def intersect_ray_sphere(camera_position, direction, sphere):
 
     return t1, t2
 
+# Функция вычисления освещенности точки
+def compute_lighting(point_on_sphere, normal_to_point):
+    # Коэффициент яркости
+    i = 0.
+
+    # Перебираем все источники освещения
+    for light in scene.lights:
+        # Если источник освещения - общий свет, то
+        # просто прибавляем к коэффициенту значение общего света
+        if light.type_of_light == 1:
+            i += light.intensity
+        else:
+            # Иначе вычисляем направление луча света
+            if light.type_of_light == 2:
+                light_ray_direction = light.position - point_on_sphere
+            else:
+                light_ray_direction = light.direction
+
+            l_dot_n = np.dot(light_ray_direction, normal_to_point)
+
+            if l_dot_n > 0:
+                # Если угол падения луча света меньше 90 градусов
+                # (если больше, значит свет достигает задней части поверхности),
+                # то вычисляем коэффициент как интенсивность света
+                # умноженное на косинус угла падения луча.
+                i += light.intensity*l_dot_n/(norm(normal_to_point) * norm(light_ray_direction))
+
+    return i
 
 # Главная функция трассировки
 def trace_ray(camera_position, direction, t_min, t_max):
     closest_t = t_max
     closest_obj = None
 
-    for obj in scene:
-        t1, t2 = intersect_ray_sphere(camera_position, direction, obj)
+    for obj in scene.objects:
+        t1, t2 = intersect_ray_sphere(camera_position, direction, obj.object_on_scene)
         if t_min <= t1 < t_max and t1 < closest_t:
             closest_t = t1
             closest_obj = obj
@@ -94,7 +153,11 @@ def trace_ray(camera_position, direction, t_min, t_max):
     if closest_obj is None:
         return BACKGROUND_COLOR
 
-    return closest_obj.color
+    point_on_sphere = camera_position + closest_t * direction
+    normal_to_point = point_on_sphere - closest_obj.object_on_scene.center
+    normal_to_point = normal_to_point / norm(normal_to_point)
+    return closest_obj.object_on_scene.color * compute_lighting(point_on_sphere, normal_to_point)
+
 
 
 while True:
